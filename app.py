c7cc7364c5af2f2e384e7d180701507f3fcf2a3b
musubu76 <t125072@ed.sus.ac.jp>
2026-01-28 19:47:47 +0900

    概要

--- app.py
+++ app.py
@@ -4,18 +4,28 @@ import plotly.express as px
 
 st.title('大学卒業予定者の就職状況')
 
+# st.expander
+with st.expander('概要・目的'):
+    st.write('概要:1996年から2025年までの大学卒業予定者における就職状況を可視化した分析ツールです。')
+    st.write('目的:年度ごとの比較や、時系列での変化を分析することで、約30年間の就職状況への理解を深めます。')
+
 df = pd.read_csv('FEH_00400402_260126104754.csv', encoding='shift_jis')
 
 df['value'] = pd.to_numeric(df['value'], errors='coerce')
 
 st.header('1. 年度別比較')
 
+# st.expander
+with st.expander('操作方法'):
+    st.write('左側にある「1.年度設定」から、年度を選択してください。')
+
 # 全体(大学):130、全体(性別):100
 df_basic = df[(df['cat01_code'] == 130) & (df['cat02_code'] == 100)]
 
 # 年度選択
+st.sidebar.header('1.年度設定')
 year_list = df_basic['時間軸(10月)'].unique()
-selected_year = st.sidebar.selectbox('1.年度を選択してください', year_list)
+selected_year = st.sidebar.selectbox('年度を選択してください', year_list)
 
 df_year = df_basic[df_basic['時間軸(10月)'] == selected_year]
 
@@ -23,14 +33,15 @@ df_year = df_basic[df_basic['時間軸(10月)'] == selected_year]
 wish_rate = df_year[df_year['tab_code'] == 100]['value'].values[0]
 offer_rate = df_year[df_year['tab_code'] == 120]['value'].values[0]
 
-st.write(f'### {selected_year} 就職希望率と内定率の比較')
+st.write(f'### {selected_year} 就職希望率vs内定率 (全体)')
 
 # st.metric
 col1, col2 = st.columns(2)
 col1.metric('就職希望率', f'{wish_rate}%')
 col2.metric('就職内定率', f'{offer_rate}%')
 
-st.write(f'### {selected_year} 国公立vs私立 内定率の比較')
+# 就職希望率:100、就職内定率:120、国公立:140、私立:150
+st.write(f'### {selected_year} 国公立vs私立 (内定率の比較)')
 df_compare = df[(df['時間軸(10月)'] == selected_year) & 
                 (df['cat02_code'] == 100) & 
                 (df['tab_code'] == 120) & 
